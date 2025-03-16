0ad7e28b8c3c61b5bdd9d46109dc8c2e93d9d91d
wangheng <18810900225@163.com>
2025-03-16 18:26:07 +0800

    拦截登录 验证码

--- bbs/middleware/auth.py
+++ bbs/middleware/auth.py
@@ -8,11 +8,11 @@ class AuthMiddleware(MiddlewareMixin):
     def process_request(self, request):
         # 1、排除不需要鉴权的页面
         # request.path_info获取当前访问文件URL
-        if request.path_info in ['/bbs/login/','/image/code/','/api/']:
+        if request.path_info in ['/bbs/login/','/image/code/','/api/','/index/login/']:
             return
             # 2、读取当前用户的session信息，如果能读到，说明能访问
         info_dict = request.session.get("info")
         if info_dict:
             return
         # 3、没有登录过，返回登录界面
-        return redirect('/bbs/login/')
+        return redirect('/index/login/')
