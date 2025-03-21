bb6df6e8c8d4e04df3227cf10fdedc97323c7f82
wangheng <18810900225@163.com>
2025-03-21 12:58:39 +0800

    可下载运行

--- customer/settings.py
+++ customer/settings.py
@@ -36,8 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "bbs.apps.BbsConfig",
-    "captcha" #验证码
+    "bbs.apps.BbsConfig"
 ]
 
 MIDDLEWARE = [
