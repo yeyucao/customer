afce46d4db0452487c64c4d3425dd920bea855e5
wangheng <18810900225@163.com>
2025-03-21 12:57:17 +0800

    可下载运行

--- customer/settings.py
+++ customer/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
-from win32con import FLASHW_ALL
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
